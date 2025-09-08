0824bbca3f3fc2b6ce72eef615947901707d3725
anugrahshinde23 <andyy5325@gmail.com>
2025-09-08 12:46:33 +0530

    made changes in firebase.js

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // Parse JSON content directly from environment variable
-const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_JSON);
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
