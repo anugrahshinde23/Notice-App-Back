929b4a7a813098a9a382ce6c45179ea40fb07d07
anugrahshinde23 <andyy5325@gmail.com>
2025-09-08 12:26:35 +0530

    health route created

--- db.js
+++ db.js
@@ -8,4 +8,13 @@ const pool = new Pool({
     ssl : {rejectUnauthorized : false} // sometimes required on hosted pg
 });
 
+pool.query('SELECT NOW()', (err, res) => {
+    if(err){
+        console.error('DB Connection Error:', err);
+    } else {
+        console.log('DB Connected:', res.rows[0]);
+    }
+    
+});
+
 module.exports = pool
