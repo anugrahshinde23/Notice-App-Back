7eecec81d9d6cc9e0eeb754c792429c7bcb03bab
anugrahshinde23 <andyy5325@gmail.com>
2025-09-08 13:26:33 +0530

    changes in firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ const admin = require("firebase-admin");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Parse the JSON from .env
-let serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_JSON);
+// Parse service account JSON from .env
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_JSON);
 
-// Replace escaped \n with actual newlines
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+// Replace escaped \n with real newlines
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
-// Initialize Firebase Admin SDK
+// Initialize Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
