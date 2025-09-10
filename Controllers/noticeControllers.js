7628dfd5eb08d228e81a114e2fb1d9b22c81384c
anugrahshinde23 <andyy5325@gmail.com>
2025-09-10 20:45:40 +0530

    added translation logic

--- Controllers/noticeControllers.js
+++ Controllers/noticeControllers.js
@@ -7,7 +7,8 @@ const categorizeNotice = require("../functions/categorizeNotice");
 const { measureMemory } = require("vm");
 const admin = require('../firebase');
 const { response } = require("express");
-const translate = require('@vitalets/google-translate-api').default
+const translateModule = await import('@vitalets/google-translate-api');
+const translate = translateModule.default;
 
 // Uploads folder ensure karna
 const uploadDir = path.join(__dirname, "..", "uploads");
