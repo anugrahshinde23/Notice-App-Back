4ec76b0adc6058d4862119d00f4b46c3b1c6a229
anugrahshinde23 <andyy5325@gmail.com>
2025-09-13 17:05:03 +0530

    fix db path

--- Middlewares/attendanceMiddleware.js
+++ Middlewares/attendanceMiddleware.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const QRCode = require('qrcode');
-const db = require('db');
+const db = require("../db")
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 
