8cf8d542a21218f3308c557ee2008b786a9d793b
anugrahshinde23 <andyy5325@gmail.com>
2025-09-15 18:43:42 +0530

    added choose class option at teacher side

--- Routes/collegesRoutes.js
+++ Routes/collegesRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { getColleges, getClasses, getClassesForAttendance } = require('../Controllers/collegesControllers');
+const authMiddleware = require('../Middlewares/authMiddleware');
 const router = express.Router();
 
 
 router.get('/colleges',getColleges);
 router.get('/classes/:college_id',getClasses);
-router.get('/classes',getClassesForAttendance)
+router.get('/classes',authMiddleware,getClassesForAttendance)
 
 module.exports = router;
