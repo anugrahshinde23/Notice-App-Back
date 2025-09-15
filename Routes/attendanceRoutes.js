8426592916f9f8912edb9e171965f10051c03257
anugrahshinde23 <andyy5325@gmail.com>
2025-09-15 16:12:22 +0530

    fixed the lecture creating logic

--- Routes/attendanceRoutes.js
+++ Routes/attendanceRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/generate-qr',authenticateTeacher,qrGenerate);
 router.post('/marked',authenticateUser,qrScan);
-router.post('/create',createLecture)
+// router.post('/create',createLecture)
 router.get('/teacher-report/:lectureId',authMiddleware, getLectureReports)
 
 module.exports  = router
