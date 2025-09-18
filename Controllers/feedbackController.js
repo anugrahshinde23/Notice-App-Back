ab88049914e3a21c76ac134882cd7a7a2574d448
anugrahshinde23 <andyy5325@gmail.com>
2025-09-18 21:27:47 +0530

    added the feedback routes

--- Controllers/feedbackController.js
+++ Controllers/feedbackController.js
@@ -2,8 +2,8 @@ const db = require('../db');
 
 const feedBack = async (req,res) =>{
     try {
-        const {feedback } = req.body
-    const result = await db.query(`INSERT INTO feedback (feedback) VALUES ($1) `,[feedBack])
+        const {feedback} = req.body
+    const result = await db.query(`INSERT INTO feedback (feedback) VALUES ($1) `,[feedback])
     res.status(200).json({message : "get feedback successfully"});
     } catch (err) {
         console.error(err)
