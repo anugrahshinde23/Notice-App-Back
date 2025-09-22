4249a4353dcb5af45baa067b8447a5ce6090d347
anugrahshinde23 <andyy5325@gmail.com>
2025-09-22 15:22:38 +0530

    added the reset password logic

--- Controllers/feedbackController.js
+++ Controllers/feedbackController.js
@@ -1,4 +1,5 @@
 const db = require('../db');
+const bcrypt = require('bcrypt')
 
 const feedBack = async (req, res) => {
   try {
@@ -23,4 +24,28 @@ const feedBack = async (req, res) => {
   }
 };
 
-module.exports = { feedBack };
+const resetPassword = async(req,res) => {
+  
+    const {username, newPassword} = req.body;
+    if(!username || !newPassword){
+      return res.status(400).json({message : "username and password required"})
+    }
+
+    try {
+      const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+
+      const result = await db.query(`UPDATE users SET password=$1 WHERE name=$2 RETURNING*`,[hashedNewPassword,username]);
+      if(result.rowCount == 0){
+        return res.status(400).json({message : "User Not Found"})
+      }
+
+      res.status(200).json({message : "Password Updated successfully"})
+    } catch (error) {
+      console.error(err);
+      res.status(500).json({ message: "Server error" });
+    }
+
+ 
+}
+
+module.exports = { feedBack, resetPassword};
--- Routes/feedbackRoutes.js
+++ Routes/feedbackRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { feedBack } = require('../Controllers/feedbackController');
+const { feedBack, resetPassword } = require('../Controllers/feedbackController');
 const router = express.Router();
 
 router.post('/save-feedback',feedBack)
+router.post('/reset-password', resetPassword)
 
 module.exports = router
